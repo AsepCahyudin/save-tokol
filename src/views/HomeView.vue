d00ad3081dd6ed5c80d17549396b00f8dc90e11d
unknown <asep.cahyudin92@gmail.com>
2022-12-22 06:27:21 +0700

    add assets

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="home">
     <h1>This is an home page</h1>
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Selamat data di save tokol"/> -->
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Selamat data di save tokol"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    // HelloWorld
+    HelloWorld
   }
 }
 </script>
