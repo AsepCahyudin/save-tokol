f8be5b520db50ef4bbed0d95d7ab5d96af69d3f1
unknown <asep.cahyudin92@gmail.com>
2022-12-25 17:24:05 +0700

    fix carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
         <carousel :items-to-show="1">
         
         <slide class="single-hero-items set-bg" style="background-image: url('/img/hero-1.jpg')">
-            <div class="container">
+            <div class="container" style="height: 500px;">
                 <div class="row">
                     <div class="col-lg-5">
                         <span>Bag,kids</span>
@@ -21,7 +21,7 @@
             </div>
         </slide>
         <slide class="single-hero-items set-bg" style="background-image: url('/img/hero-2.jpg')">
-            <div class="container">
+            <div class="container" style="height: 500px;">
                 <div class="row">
                     <div class="col-lg-5">
                         <span>Bag,kids</span>
@@ -34,7 +34,20 @@
                 </div>
             </div>
         </slide>
-
+        <slide class="single-hero-items set-bg" style="background-image: url('/img/hero-3.jpg')">
+            <div class="container" style="height: 500px;">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <span>Bag,kids</span>
+                        <h1>Black friday</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                        </p>
+                        <a href="#" class="primary-btn">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+        </slide>
     <template #addons>
       <navigation />
       <pagination />
